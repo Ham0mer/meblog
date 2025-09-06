bc6de7a04cd02f9563395e1ba635171d22c5e360
Hammer <oio_qwq@proton.me>
2025-09-06 17:55:11 +0800

    Update site URL in astro.config.mjs to point to the new wiki site

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rainbow-jalebi-28dcb1.netlify.app",
+	site: "https://wiki.jk.sb",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
