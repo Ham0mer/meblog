e3f9e9fe9342bf82874d56675fefc4892d54fd14
Hammer <oio_qwq@proton.me>
2025-09-06 17:51:08 +0800

    Update site URL in astro.config.mjs to reflect the new Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://doc.jk.sb/",
+	site: "https://rainbow-jalebi-28dcb1.netlify.app",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
